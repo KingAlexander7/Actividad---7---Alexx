4b375f2695891c7f113713998a2b9186018e03cf
Alexander Cardona <rcardonab1@miumg.edu.gt>
2023-03-23 23:43:27 -0600

    Actividad - 7

---
+++ Notas3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+struct Estudiante{
+	int codigo;
+	string nombre;
+	int nota;
+};
+
+main(){
+Estudiante estudiante;
+	cout<<"Ingrese Codigo: ";
+	cin>>estudiante.codigo;
+	cin.ignore();
+	cout<<"Ingrese Nombre Completo: ";
+	getline(cin,estudiante.nombre);
+	cout<<"Ingrese Nota: ";
+	cin>>estudiante.nota;
+	
+	
+	cout<<"Codigo: "<<estudiante.codigo<<endl; 
+	cout<<"Estudiante: "<<estudiante.nombre<<endl;
+	cout<<"Nota: "<<estudiante.nota<<endl;
+	system("PAUSE");
+
+	
+}
